b4c761349fe0e65a0ec2a98fc4ec083dba6222de
DiegoPSantana <diego.fourstage@gmail.com>
2020-11-04 19:07:01 -0300

    Funcionalidade pulo pra direita e esquerda

--- src/Princesa.java
+++ src/Princesa.java
@@ -17,6 +17,13 @@ public class Princesa {
     private BufferedImage princesaRun[];
     private int runATual;
 
+    // Pulo
+
+    private BufferedImage princesePulo[];
+    private int puloAtual;
+    private int altutaPulo;
+    private boolean pulo;
+
     public Princesa(){
         posX = 200;
         posY = 350;
@@ -42,7 +49,7 @@ public class Princesa {
         }
         // Run
         princesaRun = new BufferedImage[20];
-        runATual = 0;a
+        runATual = 0;
         try {
             for (int i = 0; i < 20; i++) {
                 String run = "src/Sprites/Run (" + (i + 1) + ").png";
@@ -54,6 +61,23 @@ public class Princesa {
             System.out.println(e + "Error");
         }
 
+        // Pulo
+        princesePulo = new BufferedImage[30];
+        puloAtual = 0;
+        pulo = false;
+        altutaPulo = 5;
+
+        try{
+            for(int i = 0; i < 30; i++){
+                String pulo = "src/Sprites/Jump (" + (i + 1) + ").png";
+                princesePulo[i] = ImageIO.read(new File(pulo));
+                System.out.println("Imagens " + pulo + "Carregadas");
+            }
+
+        } catch (Exception e){
+            System.out.println("Imagens " + pulo + "Carregadas");
+        }
+
 
     }
 
@@ -63,26 +87,38 @@ public class Princesa {
         //System.out.println("ultimaDirecao " + ultimarDirecao);
 
         if(ultimarDirecao == 1){
-            if(direcao == 1){
-                System.out.println("RUN >>>>>>>>");
-                g.drawImage(princesaRun[runATual], posX, posY, posX + largura, posY + altura, 0, 0,
-                        princesaRun[runATual].getWidth(), princesaRun[runATual].getHeight(), null );
+            if(pulo){
+                g.drawImage(princesePulo[puloAtual], posX, posY, posX + largura, posY + altura, 0, 0,
+                        princesePulo[puloAtual].getWidth(),
+                        princesePulo[puloAtual].getHeight(), null );
             } else {
-                System.out.println("IDLE >>>>>>>>");
-                g.drawImage(princesaIdle[idleAtual], posX, posY, posX + largura, posY + altura, 0, 0,
-                        princesaIdle[idleAtual].getWidth(), princesaIdle[idleAtual].getHeight(), null );
+                if(direcao == 1){
+                    //System.out.println("RUN >>>>>>>>");
+                    g.drawImage(princesaRun[runATual], posX, posY, posX + largura, posY + altura, 0, 0,
+                            princesaRun[runATual].getWidth(), princesaRun[runATual].getHeight(), null );
+                } else {
+                    //System.out.println("IDLE >>>>>>>>");
+                    g.drawImage(princesaIdle[idleAtual], posX, posY, posX + largura, posY + altura, 0, 0,
+                            princesaIdle[idleAtual].getWidth(), princesaIdle[idleAtual].getHeight(), null );
+                }
             }
         }
 
         if(ultimarDirecao == -1){
-            if(direcao == -1){
-                System.out.println("RUN <<<<<<<<<");
-                g.drawImage(princesaRun[runATual], posX, posY, posX + largura, posY + altura,
-                        princesaRun[runATual].getWidth(), 0, 0, princesaRun[runATual].getHeight(), null );
+            if(pulo){
+                g.drawImage(princesePulo[puloAtual], posX, posY, posX + largura, posY + altura,
+                        princesePulo[puloAtual].getWidth(), 0, 0,
+                        princesePulo[puloAtual].getHeight(), null );
             } else {
-                System.out.println("IDLE <<<<<<<<");
-                g.drawImage(princesaIdle[idleAtual], posX, posY, posX + largura, posY + altura,
-                        princesaIdle[idleAtual].getWidth(), 0, 0, princesaIdle[idleAtual].getHeight(), null );
+                if(direcao == -1){
+                    System.out.println("RUN <<<<<<<<<");
+                    g.drawImage(princesaRun[runATual], posX, posY, posX + largura, posY + altura,
+                            princesaRun[runATual].getWidth(), 0, 0, princesaRun[runATual].getHeight(), null );
+                } else {
+                    //System.out.println("IDLE <<<<<<<<");
+                    g.drawImage(princesaIdle[idleAtual], posX, posY, posX + largura, posY + altura,
+                            princesaIdle[idleAtual].getWidth(), 0, 0, princesaIdle[idleAtual].getHeight(), null );
+                }
             }
         }
 
@@ -92,18 +128,37 @@ public class Princesa {
     public void update() {
 
         timer++;
-        if (timer >= 2) {
-            idleAtual++;
-            if (idleAtual >= 20) {
-                idleAtual = 0;
+        if(pulo){
+            if(timer >= 2){
+                puloAtual++;
+                if(puloAtual  >= 30){
+                    puloAtual=0;
+                    pulo = false;
+                }
+                if(puloAtual < 15){
+                    posY-=altutaPulo;
+                } else {
+                    posY +=altutaPulo;
+                }
+                System.out.println(puloAtual);
+                // timer = 0;
             }
-            runATual++;
-            if (runATual >= 20) {
-                runATual = 0;
 
+
+        } else if(!pulo) {
+            if (timer >= 2) {
+                idleAtual++;
+                if (idleAtual >= 20) {
+                    idleAtual = 0;
+                }
+                runATual++;
+                if (runATual >= 20) {
+                    runATual = 0;
+
+                }
+                //System.out.println(runATual);
+                timer = 0;
             }
-            //System.out.println(runATual);
-            timer = 0;
         }
 
         if (direcao == 1) {
@@ -121,5 +176,14 @@ public class Princesa {
         } else {
             direcao = dir;
         }
+    }
+
+    public void iniciaPulo() {
+        if(pulo == false){
+            pulo = true;
+            timer = 0;
+            puloAtual = 0;
+        }
+
     }
 }
